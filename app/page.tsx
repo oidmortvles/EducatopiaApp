b08224ba780077d0544063c6a49b6ebf0b8b7f07
danielpergst <dpereyra@gstint.com.ar>
2025-03-06 02:04:13 -0300

    Se agrega la ruta /Develop para seguir el desarrollo de la web

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
 import stlyes from './home.module.css';
-import Nav from './components/Nav';
-import Header from './components/Header';
-import ButtonAction from './components/ButtonAction';
-import Content from './components/Content';
-import Community from './components/Community';
 
 const Home : React.FC = () =>{
   return(
@@ -11,19 +6,8 @@ const Home : React.FC = () =>{
     {/* <Nav/> */}
     <main className={stlyes.home}>
       
-      {/* <Header/>
-      <Content/>
-      <Community/>     
-      
-      
-      <div className={stlyes.dv}>
-            <ButtonAction colorSet={1} text='Subscribirse' />
-            <ButtonAction colorSet={2} text='+ Info' />
-            <ButtonAction colorSet={3} text='ACTION' />
-            <ButtonAction colorSet={0} text='ACTION' />
-      </div> */}
-
       <div className={stlyes.helloWord}>
+        
         <img src="./logo.png" alt="" width={300} height={"auto"}/>
         <div className={stlyes.textBox}>
           <h1>EDUCATOPIA</h1>
